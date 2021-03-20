dc550c4b0585d98fd50309f2c4b22500a68b4d74
Plamyr <58827498+Plamyr@users.noreply.github.com>
2021-03-20 20:54:08 +0300

    Add files via upload

---
+++ ClientTcp/ClientTcp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8080;
+            const string ip = "127.0.0.1";
+
+            var endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endPoint);
+            
+            Console.WriteLine("Введите сообщение: ");
+            var message = Console.ReadLine();
+
+            var buffer = Encoding.UTF8.GetBytes(message);
+            socket.Send(buffer);
+
+            Console.WriteLine("Ответ сервера: ");
+
+            buffer = new byte[256];
+            var bytes = 0;
+            var builder = new StringBuilder();
+
+            do
+            {
+                bytes = socket.Receive(buffer);
+                builder.Append(Encoding.UTF8.GetString(buffer, 0, bytes));
+            } while (socket.Available > 0);
+
+            Console.WriteLine(builder.ToString());
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+            
+        }
+    }
+}
---
+++ ServerTcp/ServerTcp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 8080;
+            const string ip = "127.0.0.1";
+
+            var endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listener.Bind(endPoint);
+            listener.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Ожидание подключений...");
+                var client = listener.Accept();
+                var builder = new StringBuilder();
+
+                var buffer = new byte[256];
+                var bytes = 0;
+
+                do
+                {
+                    bytes = client.Receive(buffer);
+                    builder.Append(Encoding.UTF8.GetString(buffer, 0, bytes));
+                }
+                while (client.Available > 0);
+
+                Console.WriteLine("Сообщение пользователя: " + builder.ToString());
+
+                string answer = "Ваше сообщение доставлено";
+                buffer = Encoding.UTF8.GetBytes(answer);
+
+                client.Send(buffer);
+
+
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+
+
+
+        }
+    }
+}
